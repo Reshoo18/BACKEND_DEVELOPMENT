e125541b57389c2f1e9cdab768cf3ff0facafdba
Reshoo-18 <reshooranjan7@gmail.com>
2025-09-05 23:54:19 +0530

    env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,7 +8,12 @@ app.get('/', (req, res) => {
 app.get('/facebook', (req, res) => {
   res.send('This is the facebook page')
 })
-
-app.listen(port, () => {
+app.get('/instagram', (req, res)=>{
+  res.send('hey this is my insta page pls follow me ithe instagram ')
+})
+app.get('/youtube', (req, res)=>{
+  res.send('hey this is my youtube channel pls subscribe it')
+})
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
