0b0eb7f57f7eb87185e4a33faf6eda0aabbb34ae
Reshoo-18 <reshooranjan7@gmail.com>
2025-09-06 23:29:12 +0530

    forn and backend mrging

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 7000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,6 +13,9 @@ app.get('/instagram', (req, res)=>{
 })
 app.get('/youtube', (req, res)=>{
   res.send('hey this is my youtube channel pls subscribe it')
+})
+app.get('/twitter', (req, res)=>{
+  res.send('hey this is my twitter pls subscribe it')
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
